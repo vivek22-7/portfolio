71441e548e7e0d9fa88f9aceb6f06454c49fcd6a
vivek22-7 <vivek22_7@live.com>
2024-04-12 18:52:35 +0530

    SkillCard and Sckill component added... started with code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
 import './App.css';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Navbar/>
     <div className="container">
       <Hero/>
+      <Skills/>
     </div>
     
     </>
---
+++ src/utils/data.js
@@ -0,0 +1,39 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "./assets/images/web-design.png",
+        skills: [
+            {skill: "HTML5", percentage: "80%"},
+            {skill: "CSS3 (Sass/SCSS)", percentage: "90%"},
+            {skill: "JavaScript", percentage: "75%"},
+            {skill: "React.js", percentage: "80%"},
+        ],
+    },
+    {
+        title: "Backend",
+        icon: "./assets/images/program.png", 
+        skills: [
+            {skill: "Node.js", percentage: "70%"}, 
+            {skill: "Express.js", percentage: "65%"},
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "./assets/images/tool.png",
+        skills: [
+            {skill: "Git & GitHub", percentage: "85%"},
+            {skill: "Visual Studio Code", percentage: "75%"},
+            {skill: "Webpack", percentage: "60%"},
+            {skill: "Responsive Design", percentage: "70%"},
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "./assets/images/professional.png",
+        skills: [
+            {skill: "Problem-solving", percentage: "80%"},
+            {skill: "Collaboration", percentage: "85%"},
+            {skill: "Attention to Detail", percentage: "75%"},
+        ],
+    },
+];
