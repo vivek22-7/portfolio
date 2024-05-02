7384b3d4d9079d2c5a1d0830682670d4b190ad2e
vivek22-7 <vivek22_7@live.com>
2024-05-02 20:44:40 +0530

    project card added with css but not complete

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './ProjectCard.css'
 
 const ProjectCard = ({details}) => {
+  
   return (
     <div className="project-card">
         <h6>{details.title}</h6>
-
-        <div className="url">{details.url}</div>
+        
+        <div className="url"> <a href="#">GitHub Link :</a>{details.url}</div>
 
         <ul>
-            {details.description.map((item) => (
+            {details.description.map((item) => ( 
                 <li key={item}>{item}</li>
             ))}
         </ul>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Projects.css'
 import { PROJECTS } from '../../utils/data'
 import ProjectCard from './ProjectCard/ProjectCard'
+import Slider from 'react-slick'
 
 const Projects = () => {
+
+  const sliderRef = useRef();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoints: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+
   return (
     <section className='projects-container'>
         <h5>Projects</h5>
 
         <div className="projects-content">
+          <div className="arrow-right">
+            <span className="material-symbols-outlined">chevron_right</span>
+          </div>
+          <div className="arrow-left">
+            <span className="material-symbols-oulined">chevron_left</span>
+          </div>
+          <Slider ref={sliderRef} {...settings}>
             {PROJECTS.map((item) => (
                 <ProjectCard key={item.title} details={item}/>
             ))}
+          </Slider>
         </div>
     </section>
   )
